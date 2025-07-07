b820f9d5b6ed6b71f2b2ade0948fb80bf960ef26
Jesus Salcedo <jesalceg@gmail.com>
2025-07-07 18:30:33 -0500

    refactor: adjust meta description and page title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,13 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
+      <Head >
         <link rel="shortcut icon" href="/favicon.png" />
+        <title>Jesus Salcedo | Software Developer</title>
+        <meta name="description" content="Portfolio of Jesus Salcedo, fullstack developer focused on Python, AI, and web technologies." />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
-    </Html>
+    </Html >
   );
 }
